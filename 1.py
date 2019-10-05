e6fe87729d896619d56af86809efff33e997331f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-10-05 11:58:05 +0900

    remove sample files

--- 1.py
+++
@@ -1,5 +0,0 @@
-import folium
-
-map = folium.Map(location=[35.681382, 139.76608399999998], zoom_start=14)
-folium.Marker([35.658581, 139.745433], popup='Tokyo tower', icon=folium.Icon(color='blue')).add_to(map)
-map.save("1.html")
