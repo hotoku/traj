236cc4f66c76321fa4a62e30b6ac148b9876104c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-10-05 11:57:56 +0900

    tiny change

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from bottle import route, run, template, static_file, request, view
 import folium
 import io
-import logging
 import sqlite3
 
 
 
 
-def setup_logger(level=logging.DEBUG):
-    LOGGER = logging.getLogger("traj")
-    LOGGER.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('traj.log')
-    formatter = logging.Formatter('%(msg)s')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    LOGGER.addHandler(file_handler)
-    return LOGGER
-
-LOGGER = setup_logger()    
 
 def get_map_html(lls):
     clat, clon = 0, 0
-    # location: 東京駅の緯度経度
-    # location, zoom_startは、本来、llsの中身に応じて調整するべきだが、
+    # location, zoom_startは、本来、llsの中身に応じて調整するべきだが、適当な値を入れている
     fmap = folium.Map(location=[35.681236, 139.767125],
                       zoom_start=20)
     for lat, lon in lls:
@@ -36,9 +23,6 @@ def get_map_html(lls):
         fmap.save(bio, close_file=False)
         ret = bio.getvalue()
     return ret
-
-
-
 
 def get_trajectory(user_id):
     dbname = "db/db.sqlite"
